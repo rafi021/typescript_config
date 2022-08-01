69f6ea02760a60c237da7ff02ce8d43125458fd6
rafi021 <mahmud.ibrahim021@gmail.com>
2022-08-01 11:45:23 +0600

    tuples in typescript

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,5 @@
 "use strict";
-let numbers = [1, 2, 3, 4, 5, 6, 7];
-numbers.forEach((n) => {
-    console.log(n.toString());
-});
-let persons = ['nodi', 'nala', 'massh', 'goat'];
-persons.forEach((item) => {
-    console.log(item, item.length);
-});
+let user = [1, 'Ibrahim'];
+console.log(user[0].toString());
+console.log(user[1].toUpperCase());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
-let numbers: number[] = [1,2,3,4,5,6,7];
-numbers.forEach((n) => {
-    console.log(n.toString())
-})
+let user: [number, string] = [1, 'Ibrahim'];
 
-let persons: string[] = ['nodi', 'nala', 'massh', 'goat'];
-persons.forEach((item) => {
-    console.log(item , item.length)
-})
+console.log(user[0].toString());
+console.log(user[1].toUpperCase());
