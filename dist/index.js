358cc14d9c1b11aca726cdb27d6ee4790aa52e17
rafi021 <mahmud.ibrahim021@gmail.com>
2022-08-01 11:42:05 +0600

    array in typescript

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,10 @@
 "use strict";
-let sales = 123456789;
-let course = 'TypeScript';
-let is_published = true;
+let numbers = [1, 2, 3, 4, 5, 6, 7];
+numbers.forEach((n) => {
+    console.log(n.toString());
+});
+let persons = ['nodi', 'nala', 'massh', 'goat'];
+persons.forEach((item) => {
+    console.log(item, item.length);
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-let sales: number = 123_456_789; // can be add "_" for a big number
-let course: string = 'TypeScript';
-let is_published: boolean = true;
-let level;  // type any, try to use any type
+let numbers: number[] = [1,2,3,4,5,6,7];
+numbers.forEach((n) => {
+    console.log(n.toString())
+})
+
+let persons: string[] = ['nodi', 'nala', 'massh', 'goat'];
+persons.forEach((item) => {
+    console.log(item , item.length)
+})
