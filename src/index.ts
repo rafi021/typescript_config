d11e5c17a99fe771f2d897d96241fd54f22109b8
rafi021 <mahmud.ibrahim021@gmail.com>
2022-08-01 11:31:36 +0600

    typescript general type

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+let sales: number = 123_456_789; // can be add "_" for a big number
+let course: string = 'TypeScript';
+let is_published: boolean = true;
+let level;  // type any, try to use any type
